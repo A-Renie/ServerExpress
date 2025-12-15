4daafa44a305e8f346cabdd4bf3b1a202c5e281e
Arthur Renié <renie.arthur@gmail.com>
2025-12-15 13:26:55 +0100

    FIX

--- app.js
+++ app.js
@@ -2,13 +2,9 @@ const express = require('express');
 const app = express();
 
 const todoRoutes = require('./src/routes/ToDoRoutes');
-// const errorHandler = require('./src/middlewares/errorHandler');
 
 app.use(express.json());
 
 app.use('/api/todos', todoRoutes);
-
-// middleware d'erreur TOUJOURS en dernier
-// app.use(errorHandler);
 
 module.exports = app;
