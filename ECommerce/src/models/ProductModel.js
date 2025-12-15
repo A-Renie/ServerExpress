3e8a5dbd2b184565e9f408709ed6a0bec9662f60
Arthur Renié <renie.arthur@gmail.com>
2025-12-15 16:35:55 +0100

    Start ProductModel

---
+++ ECommerce/src/models/ProductModel.js
@@ -0,0 +1,43 @@
+let productList = [
+    {id:1, name:"RTX 5090", price:2999.00, stock: 42, isActive:true},
+    {id:2, name:"Produit 2", price:2999.00, stock: 10000, isActive:true},
+    {id:3, name:"Produit 3", price:99.99, stock: 0, isActive:false},
+]
+
+let currentId=3
+
+class ProductModel{
+    constructor(id,name,price,stock,isActive){
+        this.id = currentId ++ ;
+        this.name = name || null ;
+        this.price = price || 0 ;
+        this.stock = stock || 0 ;
+        this.isActive = isActive || true ;
+    }
+
+    static async findAll() {
+        return productList;
+    }
+
+    // a améliorer pasque pas ouf
+    static async findById(id) {
+        let result = null
+
+        productList.forEach((product)=>{
+            if (product.id === id)
+                result = product
+        })
+
+        return result
+    }
+
+    static async create(data) {
+        const newProduct = new ProductModel(currentId++, data?.name, data?.price ,data?.stock, data?.isActive )
+        productList.push(newProduct);
+        return newProduct;
+    }
+
+
+}
+
+module.exports = ProductModel;
